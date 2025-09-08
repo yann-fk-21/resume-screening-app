8cfe7bc3732eec251113ddad7f83d5277fbe1d6d
yann-fk-21 <168545866+yann-fk-21@users.noreply.github.com>
2025-09-08 21:54:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ tfidf = joblib.load("./models/tfidf_model.pkl")
 class_map = {6: 'Data Science', 12: 'HR', 0: 'Advocate', 1: 'Arts', 24: 'Web Designing', 16: 'Mechanical Engineer', 22: 'Sales', 14: 'Health and fitness', 5: 'Civil Engineer', 15: 'Java Developer', 4: 'Business Analyst', 21: 'SAP Developer', 2: 'Automation Testing', 11: 'Electrical Engineering', 18: 'Operations Manager', 20: 'Python Developer', 8: 'DevOps Engineer', 17: 'Network Security Engineer', 19: 'PMO', 7: 'Database', 13: 'Hadoop', 10: 'ETL Developer', 9: 'DotNet Developer', 3: 'Blockchain', 23: 'Testing'}
 
 def main():
-    st.title("Resume Screening App")
+    st.title("Resume Classifier")
     upload_file = st.file_uploader("Upload your resume here", type=["pdf"])
     resume_text = ""
 
